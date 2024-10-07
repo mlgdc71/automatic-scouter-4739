783827531e61de4b0b64a7cfc753dc9853617666
chend <chendavid1919@gmail.com>
2024-10-07 12:38:56 +1100

    newest

--- track.py
+++ track.py
@@ -29,12 +29,12 @@ def callback(frame: np.ndarray, _: int) -> np.ndarray:
     annotated_frame = trace_annotator.annotate(
         annotated_frame, detections=detections)
 
-    with sv.CSVSink("tracking_results.csv") as sink:
+    with sv.CSVSink("tracking_results1.csv") as sink:
         for frame_index, frame in enumerate(frames_generator):
             sink.append(detections, {"frame_index": frame_index})
 
 sv.process_video(
     source_path="video.mp4",
-    target_path="resultc.mp4",
+    target_path="resultc1.mp4",
     callback=callback
 )
